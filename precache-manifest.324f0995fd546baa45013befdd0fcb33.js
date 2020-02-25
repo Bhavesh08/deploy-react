e591c07ce1440a775cdd8442a2d13e5f376d1e3c
Bhavesh <sonumudhiraj@gmail.com>
2020-02-25 20:06:26 +0530

    Updates

---
+++ precache-manifest.324f0995fd546baa45013befdd0fcb33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1648fda7ce184e05272068452f39e5e6",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "c94e3a87e873dbb0176d",
+    "url": "/deploy-react/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "939514d8157d1e77a749",
+    "url": "/deploy-react/static/js/2.7c8897f3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/deploy-react/static/js/2.7c8897f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94e3a87e873dbb0176d",
+    "url": "/deploy-react/static/js/main.848ec798.chunk.js"
+  },
+  {
+    "revision": "789def5c6dfd2ded82d6",
+    "url": "/deploy-react/static/js/runtime-main.4612f8e5.js"
+  }
+]);
